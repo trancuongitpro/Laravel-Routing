7672345bb1cc8df5e1c675b14441a0f9e5db26a8
cuongtran <trancuongitpro@gmail.com>
2021-06-22 23:14:40 +0700

    Update php

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    Student
+    <ul>
+        <li><a href="/admin/student/create">Student create</a>
+        </li>
+        <li><a href="/admin/student/list">Student list</a>
+        </li>
+        <li><a href="/admin/student/detail">Student byid</a>
+        </li>
+        <li><a href="/admin/student/delete">Student delete</a>
+        </li>
+        <li><a href="/admin/student/view">Student detail</a>
+        </li>
+    </ul>
+</div>
+<div>
+    <p>Teacher</p>
+    <ul>
+        <li><a href="/admin/teacher/create">Teacher create</a>
+        </li>
+        <li><a href="/admin/teacher/list">Teacher list</a>
+        </li>
+        <li><a href="/admin/teacher/detail">Teacher detail</a>
+        </li>
+        <li><a href="/admin/teacher/update">Teacher detail</a>
+        </li>
+        <li><a href="/admin/teacher/delete">Teacher detail</a>
+        </li>
+    </ul>
+</div>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route:: get('/', function (){
+    return view('homepage');
+});
+Route:: get('/admin/student/create', [\App\Http\Controllers\StudentController::class,'create']);
+Route:: get('/admin/student/list', [\App\Http\Controllers\StudentController::class,'getList']);
+Route:: get('/admin/student/detail', [\App\Http\Controllers\StudentController::class,'getById']);
+Route:: get('/admin/student/update', [\App\Http\Controllers\StudentController::class,'update']);
+Route:: get('/admin/student/delete', [\App\Http\Controllers\StudentController::class,'delete']);
+Route:: get('/admin/student/view', [\App\Http\Controllers\StudentController::class,'view']);
+
+
+
+
+Route:: get('/admin/teacher/create', [\App\Http\Controllers\TeacherController::class,'create']);
+Route:: get('/admin/teacher/list', [\App\Http\Controllers\TeacherController::class,'getList']);
+Route:: get('/admin/teacher/detail', [\App\Http\Controllers\TeacherController::class,'getById']);
+Route:: get('/admin/teacher/update', [\App\Http\Controllers\TeacherController::class,'update']);
+Route:: get('/admin/teacher/delete', [\App\Http\Controllers\TeacherController::class,'delete']);
+Route:: get('/admin/teacher/view', [\App\Http\Controllers\TeacherController::class,'view']);
+
+
+
+
+
+

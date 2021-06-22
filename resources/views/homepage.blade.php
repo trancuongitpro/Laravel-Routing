ee6380a5c6532c3f272d80cc3a1a9b89b6d91e3f
cuongtran <trancuongitpro@gmail.com>
2021-06-23 01:04:05 +0700

    Update QLSV

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -8,36 +9,50 @@
     <title>Document</title>
 </head>
 <body>
-<div>
-    Student
-    <ul>
-        <li><a href="/admin/student/create">Student create</a>
-        </li>
-        <li><a href="/admin/student/list">Student list</a>
-        </li>
-        <li><a href="/admin/student/detail">Student byid</a>
-        </li>
-        <li><a href="/admin/student/delete">Student delete</a>
-        </li>
-        <li><a href="/admin/student/view">Student detail</a>
-        </li>
-    </ul>
+<div class="row">
+    <div class="col-sm-9" ><p>Menu</p>
+
+
+        <div class="row">
+            <div class="col-xs-8 col-sm-6">
+                <div>
+                    Student
+                    <ul>
+                        <li><a href="/admin/student/create">Student create</a>
+                        </li>
+                        <li><a href="/admin/student/list">Student list</a>
+                        </li>
+                        <li><a href="/admin/student/detail">Student byid</a>
+                        </li>
+                        <li><a href="/admin/student/delete">Student delete</a>
+                        </li>
+                        <li><a href="/admin/student/view">Student detail</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xs-4 col-sm-6">
+                <div>
+                    <p>Teacher</p>
+                    <ul>
+                        <li><a href="/admin/teacher/create">Teacher create</a>
+                        </li>
+                        <li><a href="/admin/teacher/list">Teacher list</a>
+                        </li>
+                        <li><a href="/admin/teacher/detail">Teacher detail</a>
+                        </li>
+                        <li><a href="/admin/teacher/update">Teacher detail</a>
+                        </li>
+                        <li><a href="/admin/teacher/delete">Teacher detail</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-<div>
-    <p>Teacher</p>
-    <ul>
-        <li><a href="/admin/teacher/create">Teacher create</a>
-        </li>
-        <li><a href="/admin/teacher/list">Teacher list</a>
-        </li>
-        <li><a href="/admin/teacher/detail">Teacher detail</a>
-        </li>
-        <li><a href="/admin/teacher/update">Teacher detail</a>
-        </li>
-        <li><a href="/admin/teacher/delete">Teacher detail</a>
-        </li>
-    </ul>
-</div>
+
+
 
 </body>
 </html>
